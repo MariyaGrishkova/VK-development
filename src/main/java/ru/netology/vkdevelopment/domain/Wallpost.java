e1b7a4b56557cdbadfe9566cd5e2a36c958715cc
Mariya Grishkova <galeyevamv@gmail.com>
2020-12-06 18:06:30 +0300

    Initial commit

---
+++ src/main/java/ru/netology/vkdevelopment/domain/Wallpost.java
@@ -0,0 +1,18 @@
+package ru.netology.vkdevelopment.domain;
+
+public class Wallpost {
+    private int postID;
+    private String postAuthor;
+    private String postAuthorID;
+    private String postTimeDate;
+    private String postText;
+    private String imageURL;
+    private int viewQuantity;
+    private int likeQuantity;
+    private int commentQuantity;
+    private int shareQuantity;
+
+
+}
+
+}
