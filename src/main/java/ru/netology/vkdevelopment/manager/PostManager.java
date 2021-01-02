390e8fc21390f561e01327e9e95330494880d109
Mariya Grishkova <galeyevamv@gmail.com>
2021-01-02 14:57:00 +0300

    ex2 added

--- src/main/java/ru/netology/vkdevelopment/manager/PostManager.java
+++ src/main/java/ru/netology/vkdevelopment/manager/PostManager.java
@@ -18,4 +18,15 @@ public class PostManager {
     public void delete(int postID, int ownerID) {
         //to find the post in Array and delete it
     }
+
+    public void share (int ownerID, boolean CanShare){
+        //to share post to friends
+    }
+    public void commentclose (int ownerID, boolean canClose){
+        // to close the commention
+    }
+    public void commentopen (int ownerId, boolean canOpen){
+        // to open the commentation
+    }
 }
+
